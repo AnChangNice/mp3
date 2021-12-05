7a36328d0ea3db24119521e4062b898518a7123e
AnChangNice <anchangnice@gmail.com>
2021-12-05 23:11:50 +0800

    Switch blue and red bits when use DMA2D convert RGB565 to RGB888.     #The color bits red and blue not aligned at lvgl and ILI9488.
    Signed-off-by: AnChangNice <anchangnice@gmail.com>

--- Core/Src/dma2d.c
+++ Core/Src/dma2d.c
@@ -46,7 +46,7 @@ void MX_DMA2D_Init(void)
   hdma2d.LayerCfg[1].AlphaMode = DMA2D_NO_MODIF_ALPHA;
   hdma2d.LayerCfg[1].InputAlpha = 0;
   hdma2d.LayerCfg[1].AlphaInverted = DMA2D_REGULAR_ALPHA;
-  hdma2d.LayerCfg[1].RedBlueSwap = DMA2D_RB_REGULAR;
+  hdma2d.LayerCfg[1].RedBlueSwap = DMA2D_RB_SWAP;
   hdma2d.LayerCfg[1].ChromaSubSampling = DMA2D_NO_CSS;
   if (HAL_DMA2D_Init(&hdma2d) != HAL_OK)
   {
