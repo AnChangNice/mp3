344bbf53c227a03d3412bd4c49b44692ac4501f8
AnChangNice <anchangnice@gmail.com>
2021-12-25 14:38:33 +0800

    Add simple backlight/volume adjust slider for debug.
    Signed-off-by: AnChangNice <anchangnice@gmail.com>

--- APP/gui/gui.c
+++ APP/gui/gui.c
@@ -7,21 +7,22 @@
 #include "lvgl.h"
 
 #include "backlight.h"
+#include "audio.h"
 
 #include "FreeRTOS.h"
 #include "task.h"
 
-static lv_obj_t * label;
+static lv_obj_t * backlight_label;
 
-static void slider_event_cb(lv_event_t * e)
+static void backlight_slider_event_cb(lv_event_t * e)
 {
     lv_obj_t * slider = lv_event_get_target(e);
 
     Backlight_Update(lv_slider_get_value(slider));
 
     /*Refresh the text*/
-    lv_label_set_text_fmt(label, "%"LV_PRId32, lv_slider_get_value(slider));
-    lv_obj_align_to(label, slider, LV_ALIGN_OUT_TOP_MID, 0, -15);    /*Align top of the slider*/
+    lv_label_set_text_fmt(backlight_label, "backlight %"LV_PRId32, lv_slider_get_value(slider));
+    lv_obj_align_to(backlight_label, slider, LV_ALIGN_OUT_TOP_MID, 0, -15);    /*Align top of the slider*/
 }
 
 /**
@@ -31,14 +32,47 @@ void lv_backlight_control_slider(void)
 {
     /*Create a slider in the center of the display*/
     lv_obj_t * slider = lv_slider_create(lv_scr_act());
+    lv_slider_set_value(slider, 100, LV_ANIM_ON);
     lv_obj_set_width(slider, 200);                          /*Set the width*/
     lv_obj_center(slider);                                  /*Align to the center of the parent (screen)*/
-    lv_obj_add_event_cb(slider, slider_event_cb, LV_EVENT_VALUE_CHANGED, NULL);     /*Assign an event function*/
+    lv_obj_add_event_cb(slider, backlight_slider_event_cb, LV_EVENT_VALUE_CHANGED, NULL);     /*Assign an event function*/
 
     /*Create a label below the slider*/
-    label = lv_label_create(lv_scr_act());
-    lv_label_set_text(label, "0");
-    lv_obj_align_to(label, slider, LV_ALIGN_OUT_TOP_MID, 0, -15);    /*Align top of the slider*/
+    backlight_label = lv_label_create(lv_scr_act());
+    lv_label_set_text(backlight_label, "backlight 100");
+    lv_obj_align_to(backlight_label, slider, LV_ALIGN_OUT_TOP_MID, 0, -15);    /*Align top of the slider*/
+}
+
+static lv_obj_t * vol_label;
+
+static void vol_slider_event_cb(lv_event_t * e)
+{
+    lv_obj_t * slider = lv_event_get_target(e);
+
+    Audio_SetVolume(lv_slider_get_value(slider));
+
+    /*Refresh the text*/
+    lv_label_set_text_fmt(vol_label, "vol %"LV_PRId32, lv_slider_get_value(slider));
+    lv_obj_align_to(vol_label, slider, LV_ALIGN_OUT_TOP_MID, 0, -15);    /*Align top of the slider*/
+}
+
+/**
+ * Create a slider and write its value on a label.
+ */
+void lv_player_vol_control_slider(void)
+{
+    /*Create a slider in the center of the display*/
+    lv_obj_t * slider = lv_slider_create(lv_scr_act());
+    lv_slider_set_value(slider, 100, LV_ANIM_ON);
+    lv_obj_set_width(slider, 200);                          /*Set the width*/
+    //lv_obj_center(slider);                                  /*Align to the center of the parent (screen)*/
+    lv_obj_align_to(slider, backlight_label, LV_ALIGN_OUT_BOTTOM_MID, 0, 100);
+    lv_obj_add_event_cb(slider, vol_slider_event_cb, LV_EVENT_VALUE_CHANGED, NULL);     /*Assign an event function*/
+
+    /*Create a label below the slider*/
+    vol_label = lv_label_create(lv_scr_act());
+    lv_label_set_text(vol_label, "vol 100");
+    lv_obj_align_to(vol_label, slider, LV_ALIGN_OUT_TOP_MID, 0, -15);    /*Align top of the slider*/
 }
 
 
@@ -53,6 +87,7 @@ static void gui_task(void *params)
 
     //widgets init.
     lv_backlight_control_slider();
+    lv_player_vol_control_slider();
 
     for(;;)
     {
